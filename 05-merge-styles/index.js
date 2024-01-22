76436676da496da27d77fd2b8fc391afb6873571
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-01-23 00:12:58 +0100

    feat: 05-merge-styles is done (old solution) (Tue, Jan 23, 2024 12:12:58 AM)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const style = path.join(__dirname, 'styles');
+
+fs.readdir(style, (error, files) => {
+  if (error) console.log(`Error: ${error.message}\n`);
+
+  files.forEach((file) => {
+    if (path.extname(file) == '.css') {
+      fs.readFile(
+        path.join(__dirname, 'styles', file),
+        'utf8',
+        function (error, fileContent) {
+          if (error) console.log(`Error: ${error.message}\n`);
+
+          fs.appendFile(
+            path.join(__dirname, './project-dist/bundle.css'),
+            fileContent,
+            function (error) {
+              if (error) console.log(`Error: ${error.message}\n`);
+            },
+          );
+        },
+      );
+    }
+  });
+});
+
+console.log('Success!');
